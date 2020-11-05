177dfa3f0dbbdfec2da79e96a938ffff737ffa82
Spawni <spawni888@gmail.com>
2020-11-05 04:59:27 +0300

    init

---
+++ frontend/src/views/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home">
+    Hello
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: 'Home',
+  components: {
+  },
+};
+</script>
+<style scoped lang="scss">
+
+</style>
